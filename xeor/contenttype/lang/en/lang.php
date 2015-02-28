21d81c298c66d39ec3ab64a47d5c5312787d03a2
Sozonov Alexey <sozonov.alexey@gmail.com>
2015-02-28 21:25:43 +0300

    Add Content Type plugin lang file

---
+++ xeor/contenttype/lang/en/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Content Type',
+        'description' => 'Return data in custom format.',
+    ],
+    'form' => [
+        'name' => 'Content Type',
+        'comment' => '',
+    ],
+];
